3e8cfb1bd9eea7eaa202b599cb4c94ceb70fa8c0
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-09-01 16:46:54 -0400

    Beautified everythging!

--- app/scripts/libs/pointerInteractions.js
+++ app/scripts/libs/pointerInteractions.js
@@ -1,169 +1,209 @@
 define([], function () {
 
-    (function () {
-      function CustomEvent ( event, params ) {
-        params = params || { bubbles: false, cancelable: false, detail: undefined };
-        var evt = document.createEvent( 'CustomEvent' );
-        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-        return evt;
-       }
-    
-      CustomEvent.prototype = window.CustomEvent.prototype;
-    
-      window.CustomEvent = CustomEvent;
-    })();
+	(function () {
+		function CustomEvent(event, params) {
+			params = params || {
+				bubbles: false,
+				cancelable: false,
+				detail: undefined
+			};
+			var evt = document.createEvent('CustomEvent');
+			evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+			return evt;
+		}
 
-    function Stack() { try { throw Error() } catch(ex) { return ex.stack } };
+		CustomEvent.prototype = window.CustomEvent.prototype;
 
-    return function (listenTarget) {
-        var oldPos = {x:0,y:0};
-        var debounce = Date.now();
-        var dragTimeout = Date.now();
-        var el = listenTarget;
-        var dragTriggerRadius = 10;
-        var disableMouseEvents = false;
+		window.CustomEvent = CustomEvent;
+	})();
 
-        //boolean for wehther users is currently touching, dragging
-        var interaction = false;
-        var interactionStart = 0;
-        var interactionStartLocation = {};
-        var dragging = false;
+	function Stack() {
+		try {
+			throw Error()
+		}
+		catch (ex) {
+			return ex.stack
+		}
+	};
 
-        var getEventLocation = function (e) {
-            var newPos;
-            if (e.clientX) newPos = {x: e.clientX, y: e.clientY};
-            if (e.touches && e.touches.length > 0) {
-                newPos = {x: e.touches[0].clientX, y: e.touches[0].clientY};
-            }
-            if (newPos === undefined) return;
-            return newPos;
-        };
+	return function (listenTarget) {
+		var oldPos = {
+			x: 0,
+			y: 0
+		};
+		var debounce = Date.now();
+		var dragTimeout = Date.now();
+		var el = listenTarget;
+		var dragTriggerRadius = 10;
+		var disableMouseEvents = false;
 
-        var mousemove = function (e) {
-            if(Date.now() - debounce <= 16) return;
-            var newPos = getEventLocation(e);
-            if (newPos === undefined) return;
-            oldPos = newPos;
-            var event = new window.CustomEvent('pi-move', {'detail': newPos});
-            el.dispatchEvent(event);
+		//boolean for wehther users is currently touching, dragging
+		var interaction = false;
+		var interactionStart = 0;
+		var interactionStartLocation = {};
+		var dragging = false;
 
-            var distance = Math.sqrt(Math.pow(newPos.x - interactionStartLocation.x, 2) + Math.pow(newPos.y - interactionStartLocation.y, 2));
-            var displacement = {x: newPos.x - interactionStartLocation.x, y: newPos.y - interactionStartLocation.y};
-            if(distance >= dragTriggerRadius) {
-                var eventDrag = new window.CustomEvent('pi-drag', {'detail': {current: newPos, distance: distance, displacement: displacement}});
-                if (dragging) {
-                    if (Date.now() - dragTimeout > 100) {
-                        mouseup(e, true);
-                        return;
-                    }
-                    dragTimeout = Date.now();
-                }
-                el.dispatchEvent(eventDrag);
-                if (!dragging) {
-                    dragTimeout = Date.now();
-                    dragging = true;
-                    var event2 = new window.CustomEvent('pi-dragStart', {'detail': newPos});
-                    el.dispatchEvent(event2);
-                }
-            }
-        };
-        
-        var mousedown = function (e) {
-            var newPos = getEventLocation(e);
-            interaction = true;
-            interactionStart = Date.now();
-            interactionStartLocation =  {x: newPos.x, y: newPos.y};
-            oldPos = interactionStartLocation;
-        };
+		var getEventLocation = function (e) {
+			var newPos;
+			if (e.clientX) newPos = {
+				x: e.clientX,
+				y: e.clientY
+			};
+			if (e.touches && e.touches.length > 0) {
+				newPos = {
+					x: e.touches[0].clientX,
+					y: e.touches[0].clientY
+				};
+			}
+			if (newPos === undefined) return;
+			return newPos;
+		};
 
-        var mouseup = function (e, noclick) {
-            if (interactionStart === 0 || interaction === false || interactionStartLocation.x === undefined) {
-                return;
-            }
-            
-            var newPos = oldPos;
-            var distance = Math.pow(newPos.x - interactionStartLocation.x, 2) + Math.pow(newPos.y - interactionStartLocation.y, 2);
-            var length = Date.now() - interactionStart;
-           
-            if (dragging === false && !noclick && length <= 500 || distance <= dragTriggerRadius) {
-                var event = new window.CustomEvent('pi-click', {'detail': newPos});
-                el.dispatchEvent(event);
-            }
-            if ( dragging === true) {
-                dragging = false;
-                var eventDragEnd = new window.CustomEvent('pi-dragEnd', {'detail': newPos});
-                el.dispatchEvent(eventDragEnd);
-            }
-            interaction = false;
-            interactionStart = 0;
-            interactionStartLocation = {};
-        };
+		var mousemove = function (e) {
+			if (Date.now() - debounce <= 16) return;
+			var newPos = getEventLocation(e);
+			if (newPos === undefined) return;
+			oldPos = newPos;
+			var event = new window.CustomEvent('pi-move', {
+				'detail': newPos
+			});
+			el.dispatchEvent(event);
 
-        var touchMove = function (e) {
-            mousemove(e);
-            e.preventDefault();
-        };
+			var distance = Math.sqrt(Math.pow(newPos.x - interactionStartLocation.x, 2) + Math.pow(newPos.y - interactionStartLocation.y, 2));
+			var displacement = {
+				x: newPos.x - interactionStartLocation.x,
+				y: newPos.y - interactionStartLocation.y
+			};
+			if (distance >= dragTriggerRadius) {
+				var eventDrag = new window.CustomEvent('pi-drag', {
+					'detail': {
+						current: newPos,
+						distance: distance,
+						displacement: displacement
+					}
+				});
+				if (dragging) {
+					if (Date.now() - dragTimeout > 100) {
+						mouseup(e, true);
+						return;
+					}
+					dragTimeout = Date.now();
+				}
+				el.dispatchEvent(eventDrag);
+				if (!dragging) {
+					dragTimeout = Date.now();
+					dragging = true;
+					var event2 = new window.CustomEvent('pi-dragStart', {
+						'detail': newPos
+					});
+					el.dispatchEvent(event2);
+				}
+			}
+		};
 
-        var touchStart = function (e) {
-            disableMouseEvents = true;
-            mousedown(e);
-        };
+		var mousedown = function (e) {
+			var newPos = getEventLocation(e);
+			interaction = true;
+			interactionStart = Date.now();
+			interactionStartLocation = {
+				x: newPos.x,
+				y: newPos.y
+			};
+			oldPos = interactionStartLocation;
+		};
 
-        var touchEnd = function (e) {
-            mouseup(e);
-        };
-        
-        var cursorDown = function (e) {
-            if (!disableMouseEvents) {
-                mousedown(e);
-            }
-        }
-        
-        var cursorUp = function (e) {
-            if (!disableMouseEvents) {
-                mouseup(e);
-            }
-            disableMouseEvents = false;
-        }
-        
-        var touchCancel = function (e) {
-            console.warn('touchcancel');
-            mouseup(e, true);
-        };
+		var mouseup = function (e, noclick) {
+			if (interactionStart === 0 || interaction === false || interactionStartLocation.x === undefined) {
+				return;
+			}
 
-        this.getCoords = function () {
-            return oldPos;
-        };
-        
-        this.addMoveHandler = function (func) {
-            el.addEventListener('pi-move', func, false);
-        };
-        
-        this.addClickHandler = function (func) {
-            el.addEventListener('pi-click', func, false);
-        };
+			var newPos = oldPos;
+			var distance = Math.pow(newPos.x - interactionStartLocation.x, 2) + Math.pow(newPos.y - interactionStartLocation.y, 2);
+			var length = Date.now() - interactionStart;
 
-        this.addDragHandler = function (func) {
-            el.addEventListener('pi-drag', func, false);
-        };
+			if (dragging === false && !noclick && length <= 500 || distance <= dragTriggerRadius) {
+				var event = new window.CustomEvent('pi-click', {
+					'detail': newPos
+				});
+				el.dispatchEvent(event);
+			}
+			if (dragging === true) {
+				dragging = false;
+				var eventDragEnd = new window.CustomEvent('pi-dragEnd', {
+					'detail': newPos
+				});
+				el.dispatchEvent(eventDragEnd);
+			}
+			interaction = false;
+			interactionStart = 0;
+			interactionStartLocation = {};
+		};
 
-        this.addDragStartHandler = function (func) {
-            el.addEventListener('pi-dragStart', func, false);
-        };
-        
-        this.addDragEndHandler = function (func) {
-            el.addEventListener('pi-dragEnd', func, false);
-        };
+		var touchMove = function (e) {
+			mousemove(e);
+			e.preventDefault();
+		};
 
-        //Set listeners
-        listenTarget.addEventListener('mousemove', mousemove, false);
-        listenTarget.addEventListener('mousedown', cursorDown, false);
-        listenTarget.addEventListener('mouseup', cursorUp, false);
-        el.addEventListener("touchstart", touchStart, false);
-        el.addEventListener("touchend", touchEnd, false);
-        el.addEventListener("touchcancel", touchCancel, false);
-        el.addEventListener("touchleave", touchEnd, false);
-        el.addEventListener("touchmove", touchMove, false);
-        
-    };
+		var touchStart = function (e) {
+			disableMouseEvents = true;
+			mousedown(e);
+		};
+
+		var touchEnd = function (e) {
+			mouseup(e);
+		};
+
+		var cursorDown = function (e) {
+			if (!disableMouseEvents) {
+				mousedown(e);
+			}
+		}
+
+		var cursorUp = function (e) {
+			if (!disableMouseEvents) {
+				mouseup(e);
+			}
+			disableMouseEvents = false;
+		}
+
+		var touchCancel = function (e) {
+			console.warn('touchcancel');
+			mouseup(e, true);
+		};
+
+		this.getCoords = function () {
+			return oldPos;
+		};
+
+		this.addMoveHandler = function (func) {
+			el.addEventListener('pi-move', func, false);
+		};
+
+		this.addClickHandler = function (func) {
+			el.addEventListener('pi-click', func, false);
+		};
+
+		this.addDragHandler = function (func) {
+			el.addEventListener('pi-drag', func, false);
+		};
+
+		this.addDragStartHandler = function (func) {
+			el.addEventListener('pi-dragStart', func, false);
+		};
+
+		this.addDragEndHandler = function (func) {
+			el.addEventListener('pi-dragEnd', func, false);
+		};
+
+		//Set listeners
+		listenTarget.addEventListener('mousemove', mousemove, false);
+		listenTarget.addEventListener('mousedown', cursorDown, false);
+		listenTarget.addEventListener('mouseup', cursorUp, false);
+		el.addEventListener("touchstart", touchStart, false);
+		el.addEventListener("touchend", touchEnd, false);
+		el.addEventListener("touchcancel", touchCancel, false);
+		el.addEventListener("touchleave", touchEnd, false);
+		el.addEventListener("touchmove", touchMove, false);
+
+	};
 });
--- app/scripts/libs/requestAnimSingleton.js
+++ app/scripts/libs/requestAnimSingleton.js
@@ -1,35 +1,32 @@
- define([], function () {
-    function AnimRequest(taskIn) {
-        if ( AnimRequest.prototype._singletonInstance ) {
-            return AnimRequest.prototype._singletonInstance;
-        }
-        AnimRequest.prototype._singletonInstance = this;
-    
-        var requestAnimFrame = (function(){
-          return  window.requestAnimationFrame       ||
-                  window.webkitRequestAnimationFrame ||
-                  window.mozRequestAnimationFrame    ||
-                  function( callback ){
-                    window.setTimeout(callback, 1000 / 60);
-                  };
-        })();
-        
-        var doing = false;
-        var task = taskIn;
-        
-        this.start = function () {
-            doThing();
-            doing = true;
-        };
-        
-        this.stop = function () {
-            doing = false;
-        };
-        
-        var doThing = function () {
-            task();
-            requestAnimFrame(doThing);
-        };
-    }
-    return AnimRequest;
+define([], function () {
+	function AnimRequest(taskIn) {
+		if (AnimRequest.prototype._singletonInstance) {
+			return AnimRequest.prototype._singletonInstance;
+		}
+		AnimRequest.prototype._singletonInstance = this;
+
+		var requestAnimFrame = (function () {
+			return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+		})();
+
+		var doing = false;
+		var task = taskIn;
+
+		this.start = function () {
+			doThing();
+			doing = true;
+		};
+
+		this.stop = function () {
+			doing = false;
+		};
+
+		var doThing = function () {
+			task();
+			requestAnimFrame(doThing);
+		};
+	}
+	return AnimRequest;
 });
