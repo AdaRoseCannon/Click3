fcaf6e31a6ae51626e5ff3eba62e1ee83a2bf157
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-09-10 13:24:25 -0400

    git commit -am "More work" git commit -a

---
+++ app/scripts/sceneData.js
@@ -0,0 +1,21 @@
+/*global define, THREE, $*/
+define([], function () {
+    var materials = {};
+    var models = {};
+    var actors = {};
+    
+    return function () {
+        this.addMaterial = function () {
+            
+        };
+        
+        this.addModel = function () {
+            
+        };
+        
+        this.addActor = function () {
+            
+        };
+        
+    };
+});
