88a4262408f0d3825992ef863d86d37bc02a081a
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-09-12 22:58:04 +0100

    Voronoi stuff.

--- app/scripts/mapGen/main.js
+++ app/scripts/mapGen/main.js
@@ -1,5 +1,5 @@
 /* globals define, $*/
-define(['jquery'], function ($) {
+define(['jquery', 'mapGen/rhill-voronoi-core'], function ($, Voronoi) {
 	'use strict';
 	function setup () {
 		var wrapper = document.createElement('div');
@@ -13,10 +13,70 @@ define(['jquery'], function ($) {
 		var canvas = $(wrapper).find('canvas').get(0);
 		var ctx=canvas.getContext('2d');
 
-		for (var i=0; i<100; i++) {
-			ctx.fillStyle=0xFFFFFF*Math.random();
-			ctx.fillRect(400*Math.random(),400*Math.random(),1,1);
+		var bbox = {xl:0,xr:800,yt:0,yb:600};
+		var noPoints = 100;
+
+		var points = [];
+
+		var v = new Voronoi();
+
+		for (var i=0; i<noPoints; i++) {
+			points[i] = {x: bbox.xr*Math.random(), y: bbox.yb*Math.random()};
 		}
+
+		var diagram = v.compute(points, bbox);
+
+		//relax by making the new point the average of the vertices.
+		for(var cell in diagram.cells) {
+			var px = 0, py = 0, n = 0;
+			for(var halfedge in diagram.cells[cell].halfedges) {
+				n++;
+				var va = diagram.cells[cell].halfedges[halfedge].edge.va;
+				px += va.x;
+				py += va.y;
+			}
+			points[cell] = {x: px/n, y: py/n};
+		}
+		diagram = v.compute(points, bbox);
+
+		console.log(diagram);
+		render (canvas, ctx, diagram, points);
 	}
 	setup();
+
+	function render (canvas, ctx, diagram, points) {
+		ctx.globalAlpha = 1;
+		ctx.beginPath();
+		ctx.rect(0,0,canvas.width,canvas.height);
+		ctx.fillStyle = 'white';
+		ctx.fill();
+		ctx.strokeStyle = '#888';
+		ctx.stroke();
+		// voronoi
+		if (!diagram) {return;}
+		// edges
+		ctx.beginPath();
+		ctx.strokeStyle = '#000';
+		var edges = diagram.edges,
+			iEdge = edges.length,
+			edge, vertex;
+		while (iEdge--) {
+			edge = edges[iEdge];
+			vertex = edge.va;
+			ctx.moveTo(vertex.x,vertex.y);
+			vertex = edge.vb;
+			ctx.lineTo(vertex.x,vertex.y);
+		}
+		ctx.stroke();
+		// sites
+		ctx.beginPath();
+		ctx.fillStyle = '#44f';
+		var iSite = points.length;
+		while (iSite--) {
+			vertex = points[iSite];
+			ctx.rect(vertex.x-2/3,vertex.y-2/3,2,2);
+		}
+		ctx.fill();
+
+	}
 });
